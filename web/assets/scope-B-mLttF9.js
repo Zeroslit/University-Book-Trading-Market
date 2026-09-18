85e94acee03bc63424ffaac4019cd4faf8bbe0f9
Zeroslit <zhx2007115@gmail.com>
2026-09-18 20:44:44 +0800

    chore: 重新发布演示版（同步最新数据集与封面图）

---
+++ web/assets/scope-B-mLttF9.js
@@ -0,0 +1,1 @@
+import{u as c,a1 as n,z as t}from"./index-nyooUSy4.js";function a(){const o=c(),e=n(),u=t(()=>{var r;return o.needSchoolPicker?e.currentId||null:((r=o.user)==null?void 0:r.schoolId)||null}),s=t(()=>!!u.value);return{auth:o,school:e,schoolId:u,ready:s}}export{a as u};
